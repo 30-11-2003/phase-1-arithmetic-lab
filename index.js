b538fca5871ee1879c815f491528f27260e1f1f4
ShalomOrangi <145541724+30-11-2003@users.noreply.github.com>
2023-10-03 22:58:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 =31;
+let num2 =2;
+let multiply=num1*num2;
+
+let random = Math.floor(Math.random() *1000 )+1
+
+let num3 =28;
+let num4 =6;
+let mod=num3 % num4;
+
+const max = Math.max (16,17,18,19,20);
